fcbe1658b6261edba05028e79ca34c0544f21436
phamtanphat <phatdroid94@gmail.com>
2022-06-15 20:12:51 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: "QuickSan",
         primarySwatch: Colors.blue,
       ),
       routes: {
--- lib/presentations/features/sign_in/sign_in_page.dart
+++ lib/presentations/features/sign_in/sign_in_page.dart
@@ -12,11 +12,11 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return PageContainer(
-        providers: [],
-        appBar: AppBar(
-          title: Text("Sign In"),
-        ),
-        child: SignInContainer() ,
+      providers: [],
+      appBar: AppBar(
+        title: Text("Sign In"),
+      ),
+      child: SignInContainer(),
     );
   }
 }
@@ -29,9 +29,139 @@ class SignInContainer extends StatefulWidget {
 }
 
 class _SignInContainerState extends State<SignInContainer> {
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+  var isPassVisible = true;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+        child: Container(
+          color: Colors.white,
+          constraints: BoxConstraints.expand(),
+          child: Column(
+            children: [
+              Expanded(
+                  flex: 2, child: Image.asset("assets/images/ic_hello_food.png")),
+              Expanded(
+                flex: 4,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      _buildPhoneTextField(),
+                      _buildPasswordTextField(),
+                      _buildButtonSignIn(),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(child: _buildTextSignUp())
+            ],
+          ),
+        )
+    );
+  }
+
+  Widget _buildTextSignUp() {
+    return Container(
+        margin: EdgeInsets.only(left: 10, right: 10),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Don't have an account!"),
+            InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, "/sign-up");
+              },
+              child: Text("Sign Up",
+                  style: TextStyle(
+                      color: Colors.red, decoration: TextDecoration.underline)),
+            )
+          ],
+        ));
+  }
+
+  Widget _buildPhoneTextField() {
+    return Container(
+      margin: EdgeInsets.only(left: 10, right: 10),
+      child: TextField(
+        maxLines: 1,
+        controller: _emailController,
+        keyboardType: TextInputType.emailAddress,
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          fillColor: Colors.black12,
+          filled: true,
+          hintText: "Email",
+          labelStyle: TextStyle(color: Colors.blue),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide: BorderSide(width: 0, color: Colors.black12)),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide: BorderSide(width: 0, color: Colors.black12)),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide: BorderSide(width: 0, color: Colors.black12)),
+          prefixIcon: Icon(Icons.email, color: Colors.blue),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPasswordTextField() {
+    return Container(
+      margin: EdgeInsets.only(left: 10, right: 10),
+      child: TextField(
+        maxLines: 1,
+        controller: _passController,
+        obscureText: isPassVisible,
+        keyboardType: TextInputType.text,
+        textInputAction: TextInputAction.done,
+        decoration: InputDecoration(
+          fillColor: Colors.black12,
+          filled: true,
+          hintText: "PassWord",
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide: BorderSide(width: 0, color: Colors.black12)),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide: BorderSide(width: 0, color: Colors.black12)),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide: BorderSide(width: 0, color: Colors.black12)),
+          labelStyle: TextStyle(color: Colors.blue),
+          prefixIcon: Icon(Icons.lock, color: Colors.blue),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButtonSignIn() {
+    return Container(
+        margin: EdgeInsets.only(top: 20),
+        child: ElevatedButtonTheme(
+            data: ElevatedButtonThemeData(
+                style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.resolveWith((states) {
+                if (states.contains(MaterialState.pressed)) {
+                  return Colors.blue[500];
+                } else if (states.contains(MaterialState.disabled)) {
+                  return Colors.grey;
+                }
+                return Colors.blueAccent;
+              }),
+              elevation: MaterialStateProperty.all(5),
+              padding: MaterialStateProperty.all(
+                  EdgeInsets.symmetric(vertical: 5, horizontal: 100)),
+            )),
+            child: ElevatedButton(
+              child: Text("Login",
+                  style: TextStyle(fontSize: 18, color: Colors.white)),
+              onPressed: () {},
+            )));
   }
 }
-
